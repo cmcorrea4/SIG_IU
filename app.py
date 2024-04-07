08bda778cae9fb26c7985dedf96fdde47a2bc07c
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-04-07 12:58:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ with open('Mapa de Accidentalidad Vial Municipio de Medellín 2016.geojson', "r"
     data = json.load(read_file)
 
 st.title("Accidentalidad Municipio de Medellín 2016")
-# Carga el archivo GeoJSON como un GeoDataFrame
-#gdf = gpd.read_file('Mapa de Accidentalidad Vial Municipio de Medellín 2016.geojson')
 
 st.write('Se entiende por accidente de tránsito  evento, generalmente involuntario, generado al menos por un un vehículo en movimiento, que causa daños a '
          'personas y bienes involucrados en él, e igualmente afecta la normal circulación de los vehículos que se movilizan por la vía o vías comprendidas en el' 
@@ -23,6 +21,8 @@ day=[]
 hour=[]
 neig=[]
 dir=[]    
+
+# Decodificar el archivo en formato JSON (Java Script Object Notation)
 for feature in data['features']:
     coordinates = feature['geometry']['coordinates']
     dia=feature['properties']['dia']
@@ -36,11 +36,8 @@ for feature in data['features']:
     neig.append(barrio)
     dir.append(direccion)
     
-    
-#nm=100
 nm= st.slider('Selecciona el número de registros de accidentes quieres visualizar', 5, 1500)
-
-
+#Construir la tabla de datos (dataframe)
 dfLa = pd.DataFrame({'lat':La[0 : nm]})
 dfLo = pd.DataFrame({'lon':Lo[0 : nm]})
 dfdia= pd.DataFrame({'día' :day[0:nm]})
@@ -49,9 +46,12 @@ dfbarr=pd.DataFrame({'Barrio':neig[0:nm]})
 dfdir=pd.DataFrame({'Dirección':dir[0:nm]})
 df_g=pd.concat([dfLa, dfLo, dfdia, dfhor,dfdir,dfbarr], axis=1)
 
+# Mostrar la tabla de datos (dataframe)
 st.dataframe(df_g)
+#Dibujar el mapa utilizando las columnas 'lat', 'lon'.
 st.map(df_g)
 
+# Realizar un filtrado de los datos
 st.subheader('Filtrado')
 option_hour_min = st.selectbox('Selecciona filtro por Hora',
                                ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='1')
@@ -66,30 +66,7 @@ try:
    st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
 except:
     pass
-# Convierte el GeoDataFrame en un DataFrame de pandas
-#df2 = pd.DataFrame(gdf)
 
-#st.write(df2.columns[1])
-#st.write(df2.columns[2])
-#st.write(df2.columns[3])
-#st.write(df2['hora'].iloc[3])
-#st.write(df2['geometry'].iloc[2])
-#df2['geometry'].iloc[4]
-#df2['geometry'].shape
-#type(df2['geometry'])
-#st.write(df2['geometry'])
-#st.dataframe(df2.columns.values)
-
-#geometry = df2['geometry']
-
-# Convierte la serie de geometría en un DataFrame de pandas
-#df4 = pd.DataFrame(geometry)
-#st.dataframe(df4)
-
-
-df = pd.DataFrame(
-    np.random.randn(10, 2) / [50, 50] + [6.250136, -75.566067],  #6.250136 -75.566067
-    columns=['lat', 'lon'])
 
 st.map(df_filtrado)
 #st.write(df)
